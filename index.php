634338c472d23d0db06f5e85c2a60ee4039c6001
HaichH <healing.legodi@gmail.com>
2018-03-24 14:30:17 +0200

    Added new PHP tags and some styling.
    Added some employees and Birthdays too, would really love all of this to come from the database though... 
    
    So i'll be implementing from the database next time.

--- index.php
+++ index.php
@@ -7,11 +7,33 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Github Test</title>
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Days+One" />
+        <style>
+            label{
+                font-family: "Century Gothic", CenturyGothic, AppleGothic, sans-serif;
+            }
+            h1{
+                font-family: "Days One";
+                text-align:center;
+            }
+            .half{
+                width: 50%;
+            }
+        </style>
     </head>
     <body>
+        <?php echo '<h1>'.'Employee\'s Birthday\'s '.'</h2>';?>
+        <div class="half">
         <?php
-        echo 'Hello World';
+       
+        $names = array('Mandla','Andrew','Pieter','James','Marko');
+        $dates = array('13 January 1998','15 September 1996','12 April 1980','6 July 1982','8 December 1986');
+        for($i =0; $i <5;$i++){
+            echo ("<label>Name: $names[$i] "."</br></br>"."Birthday: $dates[$i]</label><hr/>");
+        }
+       
         ?>
+        </div>
     </body>
 </html>
